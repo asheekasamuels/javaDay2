8956e1647132e26d9ee74efc2d5ca061e8fa8bed
asheekasamuels <samuelsasheeka133@gmail.com>
2024-05-17 10:02:19 +0200

    day2

---
+++ script/code.js
@@ -0,0 +1,19 @@
+// Declare variables for name and surname
+let firstName = "Asheeka";
+let lastName = "Samuels";
+
+// Concatenate the two variables
+let fullName = firstName + " " + lastName;
+console.log(fullName); // Output will be "Asheeka Samuels"
+
+// Declare a variable for a number
+let numberVar = 5;
+
+// Multiply the variable by 5
+console.log(numberVar * 5); // Output will be 25
+
+// Create a constant with the value of your name
+const myName = "Asheeka";
+
+// Try to reassign the value with your surname
+myName = "Samuels"; // This will result in an error: "Assignment to constant variable"
